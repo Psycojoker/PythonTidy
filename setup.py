19bc9684daef112e1290a35bdd5c6b5ab56fbfbf
Laurent Peuch <cortex@worlddomination.be>
2012-05-02 05:56:02 +0200

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='PythonTidy',
+      version='1.22',
+      description='cleans up, regularizes, and reformats the text ofPython scripts',
+      author='Charles Curtis Rhode',
+      author_email='CRhode@LacusVeris.com',
+      url='http://www.lacusveris.com/PythonTidy/',
+      scripts=['pythontidy'],
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
